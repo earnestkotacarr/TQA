a9ad6edb7bc69e4b09e000eecb1c6875d80679e8
Earnest Carr <earnestkcarr@gmail.com>
2023-10-11 20:40:47 +0900

    Work done on 9th of Oct 2023- Georgia + Kareoke 5

--- index.js
+++ index.js
@@ -1,27 +1,88 @@
 require('dotenv').config()
+const OpenAI = require('openai')
+const postmark = require("postmark");
+
+
+
 const path = require('path')
+const openai = new OpenAI({
+  apiKey: process.env.OPENAIKEY, // defaults to process.env["OPENAI_API_KEY"]
+});
 const fastify = require(`fastify`)({
-    logger: true
+  logger: true
 })
 
+var client = new postmark.ServerClient(process.env.POSTMARKAPIKEY);
+
 fastify.register(require('@fastify/static'), {
-    root: path.join(__dirname, 'public'),
-  })
+  root: path.join(__dirname, 'public'),
+})
 
-fastify.get(`/api/`, function(request,reply){
-    let ipAddress = request.ip
-    let response = {
-        "message": "Hello two G's in a pod",
-        "ip": ipAddress
-    }
+fastify.post(`/api/`, async function (request, reply) {
+  console.log(request)
+  let transcript = request.body.transcript
+  if (!transcript) {
+    throw new Error('there is no transcript')
+  }
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        "role": "system",
+        "content":
+          `You are an evaluator of tutoring sessions for a tutoring agency. You will be given the transcript of a tutoring session.
 
-    reply.send(response)
+      Goal: Your goal is to understand and evaluate the session. You will produce 4 short reports to show various stakeholders. 
+      
+      Requirements: The summary is from the perspective of an evaluator and addressed to each respective stakeholder.
+      
+      Format: Format your output as a JSON object. The key will be the audience (one of "student", "tutor", "agency" and "parent"). The values in the object will be strings containing the summary.
+      
+      Here are the respective audiences for the four summaries: the parents of the student, the tutor, the tutoring agency's manager, and the student themselves.
+      Produce each summary accordingly.
+      They should appear in this order.
+      
+      Tone: Spartan, casual but professional, helpful. Do not over-explain
+      `},
+      {
+        "role": "user",
+        "content":
+          `IMPORTANT: each summary is from the perspective of an evaluator and addressed to each respective stakeholder.
+
+
+        Here is the tutoring transcript to process:
+        ${transcript} 
+        `},// string interpolation in javascript
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+  if (chatCompletion.choices.length == 0) {
+    throw new Error('No choices')
+  }
+  console.log(chatCompletion.choices)
+  let firstChoice = chatCompletion.choices[0]
+  var firstMessage = firstChoice.message.content
+  const parsedMessage = JSON.parse(firstMessage) // use parentheses for methods and inputs of fuctions, use curly to define blocks of code- bodies of code 
+  reply.send(parsedMessage)
+
+  let studentEmail = ""
+  let tutorEmail = ""
+  let agencyEmail = "support@tokyotutor.com"
+  let parentEmail = ""
+
+client.sendEmail({
+  "From": "earnestcarr@tokyotutor.com",
+  "To": agencyEmail,
+  "Subject": "Tutoring Quality Assurance Incorporated LLC (New look, same great taste)",
+  "TextBody": parsedMessage.agency
+});
+
+
 })
 
 fastify.listen({ port: process.env.PORT }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    // Server is now listening on ${address}
-  })
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
--- public/script.js
+++ public/script.js
@@ -1,1 +1,33 @@
 console.log("I am here!")
+
+function submitTranscript() {
+  var transcript = document.getElementById('transcript').value
+  let spinner = document.getElementById('spinner')
+  spinner.style.display = "block"
+  axios.post('/api/', {
+    transcript: transcript
+  })
+  .then(function (response) {
+    spinner.style.display = "none"
+    console.log(response);
+    let summarySection = document.getElementById('result')
+    if(response.status == 200){
+      summarySection.style.display = "block"
+    } else {
+      summarySection.style.display = "none"
+    }
+    let studentSummary = document.getElementById('summary-student')
+    let tutorSummary = document.getElementById('summary-tutor')
+    let agencySummary = document.getElementById('summary-agency')
+    let parentSummary = document.getElementById('summary-parent')
+    let results = response.data
+    studentSummary.innerText = results.student
+    agencySummary.innerText = results.agency
+    tutorSummary.innerText = results.tutor
+    parentSummary.innerText = results.parent
+  })
+  .catch(function (error) {
+    spinner.style.display = "none"
+    console.log(error);
+  });
+}
